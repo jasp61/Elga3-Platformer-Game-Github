3595457ac18a99fb557a0aca87d844d7a6d0097e
Jorgen Aspaker <jorgen.aspaker@gmail.com>
2023-12-09 15:50:23 +0100

    Slope Tiles and Bug Tracker

---
+++ bugtracker.gd
@@ -0,0 +1,6 @@
+"""
+This is the bug tracker. Let's put bugs here and say something like CLOSED or use strikethrough or something when we think it is fixed
+
+
+[FIXED] [̶0̶0̶1̶]̶ ̶-̶ ̶P̶l̶a̶y̶e̶r̶s̶ ̶s̶o̶m̶e̶t̶i̶m̶e̶s̶ ̶g̶e̶t̶ ̶s̶t̶u̶c̶k̶ ̶o̶n̶ ̶s̶l̶o̶p̶e̶s̶ ̶w̶h̶e̶n̶ ̶t̶h̶e̶i̶r̶ ̶s̶p̶e̶e̶d̶ ̶i̶s̶ ̶t̶o̶o̶ ̶s̶m̶a̶l̶l̶
+[002] - Player bounces down slopes
