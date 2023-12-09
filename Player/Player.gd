98795ca7dfe26a03d3d4bca71e932d6c6bd06a84
Tobias Soltvedt <tobsol01@gmail.com>
2023-12-09 18:19:48 +0100

    fixed casing to snake_case and fixed spacing

--- Player/Player.gd
+++ Player/Player.gd
@@ -17,22 +17,21 @@ var coyote_counter = 0
 var max_fall_speed = 2000.0
 
 # Double Jump
-var hasJumped = false
-var DoubleJumpIntensity = 0.85  # Change this variable to jump higher on the second jump
+var has_jumped = false
+var double_jump_intensity = 0.85  # Change this variable to jump higher on the second jump
 
 # Wall Jumping
-var wallJump = -800.0
-var jumpWall = 60
+var wall_jump = -800.0
+var jump_wall = 60
 
 # Wall Sliding
-var wallSlideSpeed = 120.0
+var wall_slide_speed = 120.0
 
-var Dashed = false
-var dashTimer = -0.1  # seconds between each key press for a dash ability to take activated
+var dashed = false
+var dash_timer = -0.1  # seconds between each key press for a dash ability to take activated
 
 # Crouching
 var crouching = false
-	
 	
 # Gravity
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -46,18 +45,19 @@ var lastdirection : Vector2  # Used for determining whether the idle animation s
 
 
 # Is the player next to a wall?
-func nextToWall():
-	return nextToRightWall() or nextToLeftWall()
+func next_to_wall():
+	return next_to_right_wall() or next_to_left_wall()
 
 
 # Is the player next to a wall on the player's right?
-func nextToRightWall():
+func next_to_right_wall():
 	return $RightWall.is_colliding()
 
 
 # Is the player next to a wall on the player's left?
-func nextToLeftWall():
+func next_to_left_wall():
 	return $LeftWall.is_colliding()
+
 
 func handle_crouch():
 	if Input.is_action_pressed("CrouchControl"):
@@ -66,17 +66,19 @@ func handle_crouch():
 	else:
 		crouching = false
 
+
 func handle_dash():
-	if Input.is_action_just_pressed("ui_dash") and !Dashed and !crouching:
+	if Input.is_action_just_pressed("ui_dash") and !dashed and !crouching:
 			if Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_right"):
-				Dashed = true
-				dashTimer = 0.25
+				dashed = true
+				dash_timer = 0.25
 				
-	if Dashed:
-		if dashTimer > 0:
+	if dashed:
+		if dash_timer > 0:
 			SPEED = DASH_SPEED
 		else:
-			Dashed = !is_on_floor()
+			dashed = !is_on_floor()
+
 
 func handle_jump():
 	if Input.is_action_just_pressed("ui_accept"):
@@ -86,30 +88,31 @@ func handle_jump():
 		jump_buffer_counter -= 1
 	
 	if jump_buffer_counter > 0:
-		if coyote_counter > 0 or Dashed and !hasJumped or nextToRightWall() or nextToLeftWall():
-			hasJumped = true
+		if coyote_counter > 0 or dashed and !has_jumped or next_to_right_wall() or next_to_left_wall():
+			has_jumped = true
 			velocity.y = JUMP_VELOCITY
 			jump_buffer_counter = 0
 			coyote_counter = 0
 			anim.play("Jump")
 		
 		# Wall Jump from wall on the player's right side	
-		if nextToRightWall(): # Wall Jumping
-			velocity.x = lerp(velocity.x, (velocity.x + wallJump), 0.6)
-			velocity.y -= jumpWall
+		if next_to_right_wall(): # Wall Jumping
+			velocity.x = lerp(velocity.x, (velocity.x + wall_jump), 0.6)
+			velocity.y -= jump_wall
 			animsprite.flip_h = true
 		
 		# Wall Jump from wall on the player's left side
-		if nextToLeftWall():
-			velocity.x = lerp(velocity.x, (velocity.x - wallJump), 0.6)
-			velocity.y -= jumpWall
+		if next_to_left_wall():
+			velocity.x = lerp(velocity.x, (velocity.x - wall_jump), 0.6)
+			velocity.y -= jump_wall
 			animsprite.flip_h = false
 	
 	# Different jump heights based on how long you hold the space bar
 	if Input.is_action_just_released("ui_accept"):
 		if velocity.y < 0:
 			velocity.y += 150
-			
+
+
 func handle_animation():
 	if lastdirection[0] == 1:
 		get_node("AnimatedSprite2D").flip_h = false
@@ -119,7 +122,7 @@ func handle_animation():
 	
 	if crouching:
 		anim.play("Crouch")
-	elif velocity.y > 0 and !nextToWall():
+	elif velocity.y > 0 and !next_to_wall():
 			anim.play("Fall")
 	elif direction and (direction[1] == 0):
 		if velocity.y == 0: #If the player is not jumping or falling
@@ -127,27 +130,29 @@ func handle_animation():
 	else:
 		velocity.x = lerp(velocity.x, 0.0, 0.2)
 		anim.play("Idle")
-		
+
+	
 func handle_wall_slide():	
-	if nextToWall() and velocity.y > 0:
-		velocity.y = wallSlideSpeed
-		if nextToRightWall():
+	if next_to_wall() and velocity.y > 0:
+		velocity.y = wall_slide_speed
+		if next_to_right_wall():
 			animsprite.flip_h = true
 			anim.play("Fall") #TODO: make wall sliding animations
-		if nextToLeftWall():
+		if next_to_left_wall():
 			animsprite.flip_h = false
 			anim.play("Fall") #TODO: make wall sliding animations
-			
+
+
 func handle_falling(delta):
 	if is_on_floor():
 		coyote_counter = coyote_time
-		hasJumped = false
+		has_jumped = false
 	
 	else:
 		if coyote_counter > 0:
 			coyote_counter -= 1
 
-		if dashTimer > 0:
+		if dash_timer > 0:
 			velocity.y = 0
 		else:
 			velocity.y += gravity * delta
@@ -157,8 +162,8 @@ func _physics_process(delta):
 	# Get player direction
 	direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") 
 	# Reset has_dashed when on the floor and reset coyote time
-	if dashTimer > 0:
-		dashTimer -= delta
+	if dash_timer > 0:
+		dash_timer -= delta
 	
 	handle_falling(delta)
 	
